8ea42ba5f9423c7c10ecdd4decdd7e6a3de882bf
Lok-ii <lokeshkataria.work@gmail.com>
2024-01-03 23:54:35 +0530

    updated

--- src/Components/AboutCards/AboutCard.js
+++ src/Components/AboutCards/AboutCard.js
@@ -1,6 +1,4 @@
 import "./aboutcard.css";
-import "react-circular-progressbar/dist/styles.css";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import Progressbar from "../ProgressBar/Progressbar";
 
 const AboutCard = (props) => {
@@ -29,50 +27,6 @@ const AboutCard = (props) => {
         <p className="languageName">{props.name}</p>
         <p className="prof">{props.proficiency}</p>
         <Progressbar progress={props.progress} />
-        
-        {/* <CircularProgressbar
-          value={props.progress}
-          text={`${props.progress}%`}
-          styles={{
-            // Customize the root svg element
-            root: {
-              width: "100px",
-            },
-            // Customize the path, i.e. the "completed progress"
-            path: {
-              // Path color
-              stroke: `rgba(62, 152, 199, ${props.progress / 100})`,
-              // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-              strokeLinecap: "butt",
-              // Customize transition animation
-              transition: "stroke-dashoffset 0.5s ease 0s",
-              // Rotate the path
-              transform: "rotate(0.25turn)",
-              transformOrigin: "center center",
-            },
-            // Customize the circle behind the path, i.e. the "total progress"
-            trail: {
-              // Trail color
-              stroke: "#d6d6d6",
-              // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-              strokeLinecap: "butt",
-              // Rotate the trail
-              transform: "rotate(0.25turn)",
-              transformOrigin: "center center",
-            },
-            // Customize the text
-            text: {
-              // Text color
-              fill: "#f88",
-              // Text size
-              fontSize: "16px",
-            },
-            // Customize background - only used when the `background` prop is true
-            background: {
-              fill: "#3e98c7",
-            },
-          }}
-        /> */}
       </div>
     );
   } else if (props.sectionName === "Development Skills") {
